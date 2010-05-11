e6660eddc2a1e89e8536f10882b20385bf0e3434
Pedro Belo <pedro@heroku.com>
2010-05-11 16:29:34 -0700

    more test; just trying this testing framework out

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var gnode    = require("./gnode");
 minitest.setupListeners();
 
 minitest.context('Gnode', function () {
-  this.assertion("converts numbers to hex", function (test) {
+  this.assertion("converts numbers to hex", function(test) {
     assert.equal("1", Gnode.toHex(1))
     assert.equal("fa0", Gnode.toHex(4000))
     assert.equal("fa1", Gnode.toHex(4001))
@@ -14,4 +14,16 @@ minitest.context('Gnode', function () {
     assert.equal("c350", Gnode.toHex(50000))
     test.finished()
   });
+
+  this.assertion("writes git packets, appending size", function(test) {
+    assert.equal('0005a', Gnode.pktWrite('a'))
+    assert.equal('0006ab', Gnode.pktWrite('ab'))
+    assert.equal('0014the brown fox...', Gnode.pktWrite('the brown fox...'))
+    test.finished()
+  })
+
+  this.assertion("writes a git packet flush", function(test) {
+    assert.equal('0000', Gnode.pktFlush())
+    test.finished()
+  })
 });
